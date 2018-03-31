cdbc77aebb4bd5bbdc5377ea8e7b1f9cbde25d2e
scrum-1 <scrum1@mde.tw>
2018-03-31 15:14:22 +0800

    add cmsimfly3

--- app_run.py
+++ app_run.py
@@ -15,4 +15,4 @@ if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
 else:
     # 表示在近端執行, 以 python3 wsgi.py 執行,  若採 uwsgi 則與 Openshift 運作模式相同
     #flaskapp.app.run(port=9443, debug=True, ssl_context=context)
-    flaskapp.app.run(debug=True)
+    flaskapp.app.run(port=88, debug=True)
